7121a2c13b1fce0635265b793126635cb1a31dc9
tretorn <xabiglesias7@gmail.com>
2023-10-20 10:52:41 +0200

    moved testenv

--- testenv/src/util/dbgprinter.h
+++
@@ -1,9 +0,0 @@
-#ifndef _DBGPRINTER_H
-#define _DBGPRINTER_H
-#include "stdint.h"
-
-void dbg_print(const char * str);
-void panic(const char * str);
-char* itoa(int64_t, int);
-int64_t atoi(const char * str);
-#endif
