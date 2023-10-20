f6f51873b48772db25c81b1a430600a78590c893
tretorn <xabiglesias7@gmail.com>
2023-10-20 10:27:38 +0200

    kot ready

--- source/dependencies.c
+++ source/dependencies.c
@@ -1,6 +1,10 @@
 #include "dependencies.h"
 #include "types.h"
 
+#ifdef USE_KOT
+//Kot imports go here
+#endif
+
 #ifdef USE_BMOON
 #include "../memory/heap.h"
 #include "../memory/paging.h"
@@ -16,7 +20,9 @@ char* odi_dep_itoa(s64 value, int base) {
     #ifdef USE_BMOON
     return itoa(value, base);
     #endif
-
+    #ifdef USE_KOT
+    //Kot itoa implementation
+    #endif
     //You can implement your own itoa here
 }
 
@@ -24,7 +30,9 @@ s64 odi_dep_atoi(const char * str) {
     #ifdef USE_BMOON
     return atoi(str);
     #endif
-
+    #ifdef USE_KOT
+    //Kot atoi implementation
+    #endif
     //You can implement your own atoi here
 }
 
@@ -32,7 +40,9 @@ int odi_dep_strcmp(const char* s1, const char* s2) {
     #ifdef USE_BMOON
     return strcmp(s1, s2);
     #endif
-
+    #ifdef USE_KOT
+    //Kot strcmp implementation
+    #endif
     //You can implement your own strcmp here
 }
 
@@ -40,7 +50,9 @@ int odi_dep_strncmp(const char* s1, const char* s2, int n) {
     #ifdef USE_BMOON
     return strncmp(s1, s2, n);
     #endif
-
+    #ifdef USE_KOT
+    //Kot strncmp implementation
+    #endif
     //You can implement your own strncmp here
 }
 
@@ -48,7 +60,9 @@ void odi_dep_strncpy(char *dest, const char *src, int n) {
     #ifdef USE_BMOON
     return strncpy(dest, src, n);
     #endif
-
+    #ifdef USE_KOT
+    //Kot strncpy implementation
+    #endif
     //You can implement your own strncpy here
 }
 
@@ -56,7 +70,9 @@ int odi_dep_strlen(const char* str) {
     #ifdef USE_BMOON
     return strlen(str);
     #endif
-
+    #ifdef USE_KOT
+    //Kot strlen implementation
+    #endif
     //You can implement your own strlen here
 }
 
@@ -64,7 +80,9 @@ void* odi_dep_malloc(int size) {
     #ifdef USE_BMOON
     return malloc(size);
     #endif
-
+    #ifdef USE_KOT
+    //Kot malloc implementation
+    #endif
     //You can implement your own malloc here
 }
 
@@ -72,7 +90,9 @@ void odi_dep_free(void* ptr) {
     #ifdef USE_BMOON
     free(ptr);
     #endif
-
+    #ifdef USE_KOT
+    //Kot free implementation
+    #endif
     //You can implement your own free here
 }
 
@@ -80,7 +100,9 @@ void odi_dep_map_current_memory_size(void* virtual_address, void* physical_memor
     #ifdef USE_BMOON
     map_current_memory_size(virtual_address, physical_memory, bytes);
     #endif
-
+    #ifdef USE_KOT
+    //Kot map_current_memory_size implementation
+    #endif
     //You can implement your own map_current_memory_size here
 }
 
@@ -88,7 +110,9 @@ void odi_dep_map_current_memory(void* virtual_memory, void* physical_memory) {
     #ifdef USE_BMOON
     map_current_memory(virtual_memory, physical_memory);
     #endif
-
+    #ifdef USE_KOT
+    //Kot map_current_memory implementation
+    #endif
     //You can implement your own map_current_memory here
 }
 
@@ -96,7 +120,9 @@ void odi_dep_outb(u16 port, u8 value) {
     #ifdef USE_BMOON
     outb(port, value);
     #endif
-
+    #ifdef USE_KOT
+    //Kot outb implementation
+    #endif
     //You can implement your own outb here
 }
 
@@ -104,7 +130,9 @@ void odi_dep_outw(u16 port, u16 value) {
     #ifdef USE_BMOON
     outw(port, value);
     #endif
-
+    #ifdef USE_KOT
+    //Kot outw implementation
+    #endif
     //You can implement your own outw here
 }
 
@@ -112,7 +140,9 @@ void odi_dep_outl(u16 port, u32 value) {
     #ifdef USE_BMOON
     outl(port, value);
     #endif
-
+    #ifdef USE_KOT
+    //Kot outl implementation
+    #endif
     //You can implement your own outl here
 }
 
@@ -120,7 +150,9 @@ u8 odi_dep_inb(u16 port) {
     #ifdef USE_BMOON
     return inb(port);
     #endif
-
+    #ifdef USE_KOT
+    //Kot inb implementation
+    #endif
     //You can implement your own inb here
 }
 
@@ -128,7 +160,9 @@ u16 odi_dep_inw(u16 port) {
     #ifdef USE_BMOON
     return inw(port);
     #endif
-
+    #ifdef USE_KOT
+    //Kot inw implementation
+    #endif
     //You can implement your own inw here
 }
 
@@ -136,7 +170,9 @@ u32 odi_dep_inl(u16 port) {
     #ifdef USE_BMOON
     return inl(port);
     #endif
-
+    #ifdef USE_KOT
+    //Kot itoa implementation
+    #endif
     //You can implement your own inl here
 }
 
@@ -144,7 +180,9 @@ void odi_dep_insw(u16 port, u8* buffer, int count) {
     #ifdef USE_BMOON
     insw(port, buffer, count);
     #endif
-
+    #ifdef USE_KOT
+    //Kot insw implementation
+    #endif
     //You can implement your own insw here
 }
 
@@ -152,7 +190,9 @@ void odi_dep_outsw(u16 port, u8 *buffer, int count) {
     #ifdef USE_BMOON
     outsw(port, buffer, count);
     #endif
-
+    #ifdef USE_KOT
+    //Kot outsw implementation
+    #endif
     //You can implement your own outsw here
 }
 
@@ -160,7 +200,9 @@ void odi_dep_io_wait() {
     #ifdef USE_BMOON
     io_wait();
     #endif
-
+    #ifdef USE_KOT
+    //Kot io_wait implementation
+    #endif
     //You can implement your own io_wait here
 }
 
@@ -168,7 +210,9 @@ void odi_dep_vprintf(const char* format, odi_va_list args) {
     #ifdef USE_BMOON
     vprintf(format, args);
     #endif
-
+    #ifdef USE_KOT
+    //Kot vprintf implementation
+    #endif
     //You can implement your own vprintf here
 }
 
@@ -176,7 +220,9 @@ void odi_dep_vsnprintf(char* buffer, int size, const char* format, odi_va_list a
     #ifdef USE_BMOON
     vsnprintf(buffer, size, format, args);
     #endif
-
+    #ifdef USE_KOT
+    //Kot vsprintf implementation
+    #endif
     //You can implement your own vsnprintf here
 }
 
@@ -187,6 +233,8 @@ void odi_dep_printf(const char* format, ...) {
     odi_dep_vprintf(format, args);
     odi_dep_va_end(args);
     #endif
-
+    #ifdef USE_KOT
+    //Kot printf implementation
+    #endif
     //You can implement your own printf here
 }
